46c22307bf2d239bccc94c0f3ac2d3565623398f
costya0805 <costya.co4ergin@gmail.com>
2024-07-11 20:11:55 +0500

    first

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <>
+        <Header />
+        <Main />
+    </>
+);
